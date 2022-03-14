82fa57f830e5301d3fcee5b72469f7b38dc17633
SumanChakraborty1998 <sumanchakra8145@gmail.com>
2022-03-15 01:31:39 +0530

    Chat Application with E2E encryption

---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+const CryptoJS = require("crypto-js");
+
+export const Chat = ({ socket, userName, room }) => {
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const handleSendMessage = async () => {
+    if (message !== "") {
+      const payload = {
+        room: room,
+        author: userName,
+        message: CryptoJS.AES.encrypt(
+          message,
+          process.env.REACT_APP_PRIVATE_KEY,
+        ).toString(),
+        timestamp:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      // const cipherText = await CryptoJS.AES.encrypt(
+      //     JSON.stringify(payload),
+      //     process.env.REACT_APP_PRIVATE_KEY,
+      // ).toString(),
+
+      await socket.emit("sendMessage", payload);
+      setMessageList((prev) => [...prev, payload]);
+      setMessage("");
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receiveMessage", (data) => {
+      console.log(data);
+      setMessageList((prev) => [...prev, data]);
+    });
+  }, [socket]);
+
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <h2>Live Chat</h2>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((message, index) => {
+            return (
+              <div
+                key={index}
+                className="message"
+                id={userName === message.author ? "you" : "other"}
+              >
+                <div className="message-content">
+                  <p>
+                    {CryptoJS.AES.decrypt(
+                      message.message,
+                      process.env.REACT_APP_PRIVATE_KEY,
+                    ).toString(CryptoJS.enc.Utf8)}
+                  </p>
+                </div>
+                <div className="message-meta">
+                  <p>{message.time}</p>
+                  <p>{message.author}</p>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => {
+            e.key === "Enter" && handleSendMessage();
+          }}
+        />
+
+        <button variant="contained" size="large" onClick={handleSendMessage}>
+          &#9658;
+        </button>
+      </div>
+    </div>
+  );
+};
